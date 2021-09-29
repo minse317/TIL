ac5046721f9231f8a3e5ae7ed9b5849e2fc6e294
minse317 <s2019w30@e-mirim.hs.kr>
2021-09-29 23:37:53 +0900

    Recap

--- paintjs/app.js
+++ paintjs/app.js
@@ -1,9 +1,11 @@
-const canvas =document.getElementById("jsCanvas");
+const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
 
 ctx.strokeStyle = "2c2c2c";
 ctx.lineWidth = 2.5;
-
 
 let painting = false;
 
@@ -22,19 +24,19 @@ function onMouseMove(event){
     if(!painting){
         ctx.beginPath();
         ctx.moveTo(x, y);
-    }else{
+    } else {
         ctx.lineTo(x,y);
         ctx.stroke();
     }
 }
 
-function onMouseDown(event){
+function onMouseDown(event) {
     painting = true;
 }
 
-if(canvas){
-    canvas.addEventListener("mousenter", onMouseMove);
-    canvas.addEventListener("mousedown", stopPainting);
+if (canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
