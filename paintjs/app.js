9bb204aa849a16101799d61f02b02b767cbc8c98
minse317 <s2019w30@e-mirim.hs.kr>
2021-09-30 01:17:22 +0900

    saving the image

--- paintjs/app.js
+++ paintjs/app.js
@@ -3,13 +3,16 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColors");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "2c2c2c";
-const CANCAS_SIZE = 700;
+const CANVAS_SIZE = 700;
 
-canvas.width = CANCAS_SIZE;
-canvas.height = CANCAS_SIZE;
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
 
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
@@ -44,13 +47,13 @@ function handleColorClick(event) {
     ctx.fillStyle = color;
 }
 
-function handleRangeClick(event) {
+function handleRangeChange(event) {
     const size = event.target.value;
     ctx.lineWidth = size;
 }
 
 function handleModeClick() { 
-    if(filling === true){
+    if(filling === true) {
         filling = false;
         mode.innerText = "Fill";
     } else {
@@ -61,8 +64,20 @@ function handleModeClick() {
 
 function handleCanvasClick(){
     if(filling){
-        ctx.fillRect(0, 0, CANCAS_SIZE, CANCAS_SIZE);
+        ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
     }    
+}
+
+function handleCM(event){
+    event.preventDefault();
+}
+
+function handleSaveClick(){
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJs[🎨]";
+    link.click();
 }
 
 if (canvas) {
@@ -70,7 +85,8 @@ if (canvas) {
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
-    canvas.addEventListener("click", handleCanvasClick)
+    canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
 }
 
 Array.from(colors).forEach(color => 
@@ -78,9 +94,13 @@ Array.from(colors).forEach(color =>
 );
 
 if(range) {
-    range.addEventListener("input", handleRangeClick);
+    range.addEventListener("input", handleRangeChange);
 }
 
 if(mode) {
-    mode.addEventListener("click", handleModeClick)
+    mode.addEventListener("click", handleModeClick);
+}
+
+if(saveBtn){
+    saveBtn.addEventListener("click", handleSaveClick);
 }
