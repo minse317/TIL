6ef9f2f7e639a643dea2f0114085f442a3263ab6
Minse <59506163+minse317@users.noreply.github.com>
2022-03-30 21:49:00 +0900

    Create 같은숫자는싫어.js

---
+++ Programmers/같은숫자는싫어.js
@@ -0,0 +1,11 @@
+function solution(arr)
+{
+    let answer = [];
+    for(let i =0; i< arr.length; i++){
+        if(arr[i] === arr[i+1]){
+        } else {
+            answer.push(arr[i])
+        }
+    }
+    return answer;
+}
