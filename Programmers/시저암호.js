3cc861027f0c34249e6b6445a011e2becab65744
Minse <59506163+minse317@users.noreply.github.com>
2022-04-15 22:33:35 +0900

    Create 시저암호.js

---
+++ Programmers/시저암호.js
@@ -0,0 +1,22 @@
+function solution(s, n) {
+  const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const lower = 'abcdefghijklmnopqrstuvwxyz';
+  let answer = '';
+
+  for (let i = 0; i < s.length; i++) {
+    const str = s[i];
+      
+    if (str == ' ') {
+      answer += ' ';
+      continue;
+    }
+      
+    const upperOrLower = upper.includes(str) ? upper : lower;
+    let index = upperOrLower.indexOf(str) + n;
+    if (index >= upperOrLower.length) {
+      index -= upperOrLower.length;
+    }
+    answer += upperOrLower[index];
+  }
+  return answer;
+}
