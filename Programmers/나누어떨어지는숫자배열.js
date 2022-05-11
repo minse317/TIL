2bb60e383ccdc24517cb96c7fca33cad90ef6ffd
Minse <59506163+minse317@users.noreply.github.com>
2022-05-11 21:47:37 +0900

    Create 나누어떨어지는숫자배열.js

---
+++ Programmers/나누어떨어지는숫자배열.js
@@ -0,0 +1,7 @@
+function solution(arr, divisor) {
+    var answer = [];
+    arr.map((o) => {
+        o % divisor === 0 && answer.push(o);
+    })
+    return answer.length ? answer.sort((a, b) => a - b) : [-1];
+}
