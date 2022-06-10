29a793aae56a4bf4d3adb5a4bcfe5b1b19d47021
Minse <59506163+minse317@users.noreply.github.com>
2022-06-10 20:50:22 +0900

    Create 스킬트리.js

---
+++ Programmers/스킬트리.js
@@ -0,0 +1,13 @@
+function solution(skill, skill_trees) {
+    let newArr = skill.split("");
+    let str = 0;
+    let count = 0;
+    
+    for(let i = 0; i < skill_trees.length; i++) {
+        str = skill_trees[i].split("").filter(element => newArr.includes(element)).join("");
+        if(str === skill.substring(0,str.length)) {
+            count++;
+        }
+    }
+    return count; 
+}
