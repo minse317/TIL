4942687d9d3023667aa865a2bdd2faf8465502ec
Minse <59506163+minse317@users.noreply.github.com>
2022-06-16 22:46:55 +0900

    Create 괄호변환.js

---
+++ Programmers/괄호변환.js
@@ -0,0 +1,39 @@
+function solution(p) {
+	let answer = "";
+	let left = 0 
+    let right = 0; 
+	let check = false;
+    
+	if (p.length == 0) {
+        return ""
+    }
+    
+	for (let i = 0; i < p.length; i++) {
+        
+		if (p[i] == '(') left++;
+		if (p[i] == ')') right++;
+        
+		if (right > left) check = true;
+        
+		if (left == right) { 
+          
+			if (check == true) {
+				answer += '(';
+				answer += solution(p.slice(i + 1, p.length));
+				answer += ')';
+              
+				for (let j = 1; j < i; j++) {
+					if (p[j] == ')') answer += '(';
+					if (p[j] == '(') answer += ')';
+				}
+				return answer;
+			}
+            
+			else { 
+				answer += p.slice(0, i + 1); 
+				answer += solution(p.slice(i + 1, p.length));
+				return answer;
+			}
+		}
+    }
+}
