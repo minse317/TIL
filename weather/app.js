4957a6b219c6f8cfd4b0b7a186c8b2439d58cf99
minse317 <s2019w30@e-mirim.hs.kr>
2022-07-01 00:03:33 +0900

    icon

--- weather/app.js
+++ weather/app.js
@@ -29,6 +29,46 @@ window.addEventListener('load',()=> {
           ubicacion.textContent = data.name
 
           vientoVelocidad.textContent = `${data.wind.speed} m/s`
+          
+          /*let iconCode = data.weather[0].icon
+          const urlIcon = `http://openweathermap.org/img/wn/${iconCode}.png`
+
+          console.log(urlIcon)*/
+          console.log(data.weather[0].main)
+          switch (data.weather[0].main) {
+            case 'Thunderstorm':
+              iconoAnimado.src='animated/thunder.svg'
+              console.log('Thunderstorm');
+              break;
+            case 'Drizzle':
+              iconoAnimado.src='animated/rainy-2.svg'
+              console.log('Drizzle');
+              break;
+            case 'Rain':
+              iconoAnimado.src='animated/rainy-7.svg'
+              console.log('Rain');
+              break;
+            case 'Snow':
+              iconoAnimado.src='animated/snowy-6.svg'
+              console.log('Snow');
+              break;                        
+            case 'Clear':
+              iconoAnimado.src='animated/day.svg'
+              console.log('Clear');
+              break;
+            case 'Atmosphere':
+              iconoAnimado.src='animated/weather.svg'
+              console.log('Atmosphere');
+              break;  
+            case 'Clouds':
+              iconoAnimado.src='animated/cloudy-day-1.svg'
+              console.log('Clouds');
+              break;  
+            default: 
+              iconoAnimado.src='animated/cloudy-day-1.svg'
+              console.log('basic');
+            }
+
         })
         .catch( error => {
           console.log(error)
